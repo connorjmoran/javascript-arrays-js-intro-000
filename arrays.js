713214f74aa0585b669abd5689af827605076161
Connor Moran <cmoran11@fordham.edu>
2020-06-15 15:57:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,5 +8,6 @@ function addElementToBeginningOfArray(array,element) {
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [element, array]
-}
+  array.unshift(element)
+  return array
+};
