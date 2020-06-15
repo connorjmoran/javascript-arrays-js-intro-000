89cec3239194445ffb6e71524f2d1d69074554db
Connor Moran <cmoran11@fordham.edu>
2020-06-15 16:32:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,7 @@ var chocolateBars = ['snickers',
 'skittles'];
 
 function addElementToBeginningOfArray(array,element) {
-  [element, ...array]
-  return array
+  return [element, ...array]
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
